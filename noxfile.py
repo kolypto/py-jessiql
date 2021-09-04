6642dfabbd905da5ea2f64aca43cda1084d58e14
Mark Vartanyan <kolypto@gmail.com>
2021-09-04 03:40:59 +0300

    Added mypy testing

--- noxfile.py
+++ noxfile.py
@@ -25,7 +25,7 @@ def tests(session: nox.sessions.Session, sqlalchemy=None):
         session.install(f'sqlalchemy=={sqlalchemy}')
 
     # Test
-    session.run('pytest', 'tests/', '--cov=myproject')
+    session.run('pytest', 'tests/', '--ignore=tests/test_mypy.py', '--cov=myproject')
 
 
 @nox.session(python=PYTHON_VERSIONS[-1])
