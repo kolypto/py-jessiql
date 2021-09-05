779cff9c65469cace2ef026a34ba02a18ce48f4b
Mark Vartanyan <kolypto@gmail.com>
2021-09-05 03:27:27 +0300

    nox: test without poetry (because it corrupts venvs)

--- noxfile.py
+++ noxfile.py
@@ -16,7 +16,7 @@ SQLALCHEMY_VERSIONS = [
     *(f'1.4.{x}' for x in range(14, 1 + 23)),
 ]
 GRAPHQL_CORE_VERSIONS = [
-    *(f'3.1.{x}' for x in range(1, 3 + 6)),
+    *(f'3.1.{x}' for x in range(3, 1 + 6)),
 ]
 FASTAPI_VERSIONS = [
     '0.51.0', '0.52.0', '0.53.2', '0.54.2', '0.56.1', '0.56.1', '0.57.0', '0.58.1', '0.59.0',
@@ -27,12 +27,12 @@ FASTAPI_VERSIONS = [
 @nox.session(python=PYTHON_VERSIONS)
 def tests(session: nox.sessions.Session, *, overrides: dict[str, str] = {}):
     """ Run all tests """
-    # session.install(*requirements(overrides), '.')
+    session.install(*requirements(overrides), '.')
 
-    session.install('poetry')
-    session.run('poetry', 'install')
-    if overrides:
-        session.install(*(f'{name}=={version}' for name, version in overrides.items()))
+    # session.install('poetry')
+    # session.run('poetry', 'install')
+    # if overrides:
+    #     session.run('poetry', 'run', 'pip', 'install', *(f'{name}=={version}' for name, version in overrides.items()))
 
     # Test
     session.run('pytest', 'tests/', '--ignore=tests/test_mypy.py', '--cov=jessiql')
@@ -60,22 +60,22 @@ def tests_fastapi(session: nox.sessions.Session, fastapi):
 
 
 
-# # Get requirements.txt from poetry
-# import tempfile, subprocess
-# with tempfile.NamedTemporaryFile('w+') as f:
-#     subprocess.run(f'poetry export --no-interaction --dev --format requirements.txt --without-hashes --output={f.name}', shell=True, check=True)
-#     f.seek(0)
-#     requirements_txt = f.readlines()
-#
-# def requirements(overrides: dict[str, str]) -> list[str]:
-#     # Filter requirements (to avoid conflicting requirements)
-#     requirements = [
-#         line.split(';', 1)[0]  # Example: "sqlalchemy==1.4.23; (python_version >= "2.7" and python_full_version < "3.0.0") or (python_full_version >= "3.6.0")"
-#         for line in requirements_txt
-#         if not any(line.startswith(f'{override}==') for override in overrides)
-#     ]
-#     # Merge with overrides
-#     return [
-#         *requirements,
-#         *[f'{name}=={version}' for name, version in overrides.items()]
-#     ]
+# Get requirements.txt from poetry
+import tempfile, subprocess
+with tempfile.NamedTemporaryFile('w+') as f:
+    subprocess.run(f'poetry export --no-interaction --dev --format requirements.txt --without-hashes --output={f.name}', shell=True, check=True)
+    f.seek(0)
+    requirements_txt = f.readlines()
+
+def requirements(overrides: dict[str, str]) -> list[str]:
+    # Filter requirements (to avoid conflicting requirements)
+    requirements = [
+        line.split(';', 1)[0]  # Example: "sqlalchemy==1.4.23; (python_version >= "2.7" and python_full_version < "3.0.0") or (python_full_version >= "3.6.0")"
+        for line in requirements_txt
+        if not any(line.startswith(f'{override}==') for override in overrides)
+    ]
+    # Merge with overrides
+    return [
+        *requirements,
+        *[f'{name}=={version}' for name, version in overrides.items()]
+    ]
