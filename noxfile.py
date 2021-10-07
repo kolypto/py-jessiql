b76bc31870eca38451f4c19edfd64e423c067908
Mark Vartanyan <kolypto@gmail.com>
2021-10-07 17:05:30 +0300

    Python 3.10

--- noxfile.py
+++ noxfile.py
@@ -8,10 +8,10 @@ nox.options.sessions = [
 ]
 
 # Versions
-PYTHON_VERSIONS = ['3.9']
+PYTHON_VERSIONS = ['3.7', '3.8', '3.9', '3.10']
 SQLALCHEMY_VERSIONS = [
     *(f'1.3.{x}' for x in range(7, 1 + 24)),
-    *(f'1.4.{x}' for x in range(14, 1 + 23)),
+    *(f'1.4.{x}' for x in range(14, 1 + 25)),
 ]
 
 
